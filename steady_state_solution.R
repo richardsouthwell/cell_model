3eaa578c36fe8516cd0febef91629a64b900ee5f
gustavdelius <gustav.delius@gmail.com>
2016-11-21 13:12:44 +0000

    A variant of the code that produced Figure 1 but now with a duplication rate that is everywhere finite. This means we no longer need to worry about avoiding the point x=1.

---
+++ steady_state_solution.R
@@ -0,0 +1,77 @@
+# Set parameters
+a <- 0.7; b <- 0.5; 
+alpha <- 0.85; beta <- 1;
+xa <- 0.7;  # threshold for duplication
+delta <- 0.2  # width of offspring size distribution
+xp <- 0.5 + delta/2  # x_+ is maximum size of offspring
+
+# Choose width of size brackets
+dx <- 0.0005
+x <- seq(dx, 1, dx)
+
+# Growth rate
+g <- a*x^alpha-b*x^beta
+
+# Make q nonzero only between (1-delta)/2 and (1+delta)/2
+q <- function(x) {
+    qr <- rep(0, length(x))
+    qr[abs(x-0.5)<delta/2] <- 1/delta
+    return(qr)
+}
+
+# Use a k that stays finite but is large enough to ensure that
+# almost all cells duplicate before reaching x=1
+k <- 4000*(x-xa)^4#/(1-x+0.01)
+k[1:(xa/dx)] <- 0
+
+# Define function that calculates Psi given
+# a particular mortality rate constant m0
+#
+# The function returns the Psi and the integral
+# over h/e that needs to be equal to 1
+p <- function(m0) {
+    
+    # Use constant mortality 
+    m <- rep_len(m0, length(x))
+    
+    # Calculate e(x)
+    ep <- (k+m)/g
+    # First calculate for x < xp
+    es <- rev(cumsum(rev(ep[1:(xp/dx)])))
+    # then for x > xp
+    el <- -cumsum(ep[(xp/dx+1):length(ep)])
+    # and put the results together
+    e <- exp(c(es, el)*dx)
+    
+    # Calculate h(x)
+    hp <- k*e/g
+    hp[1:(xa/dx)] <- 0
+    h <- rep_len(0, length(x))
+    for (i in 1:length(x)) {
+        h[i] <- 2*sum(hp*q(x[i]/x)/x)*dx
+    }
+    
+    he <- h/e
+    he[e==0] <- 0
+    Theta <- cumsum(he)*dx
+    b <- Theta[length(x)]
+    
+    Psi <- g[xp/dx]*e*Theta/g
+    return(list(Psi, b))
+}
+
+# Find the duplication rate constant that satisfies the
+# boundary condition
+m0 <- uniroot(function(m0) p(m0)[[2]]-1, lower=0.05, upper=10)[["root"]]
+
+# Calculate the solution
+psi <- p(m0)[[1]]
+# and plot it in the range where cells can exist
+xmin <- xa*(1-delta)/2
+plotrange <- (xmin/dx):length(x)
+par(mar=c(5,5,1,1))
+plot(x[plotrange], 
+     psi[plotrange], 
+     type="l", lwd=3,
+     xlab="x", ylab=expression(Psi(x)))
+
